6db3fae398a574f0a9874c54f6f227ec78dccbc5
Kayky Vitor Cruz <70824102+EternalQuasar0206@users.noreply.github.com>
2021-10-25 21:57:28 -0300

    Initial commit

---
+++ Controllers/MainController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+
+namespace AspImageConverter.Controllers
+{
+    [ApiController]
+    [Route("/")]
+    public class MainController : ControllerBase
+    {
+
+        [HttpGet]
+        public FileContentResult Get(string file, string output = "jpg")
+        {
+            Byte[] imageBytes = Convert.FromBase64String(file);
+            using(var stream = new MemoryStream(imageBytes, 0, imageBytes.Length))
+            {
+                Image image = Image.FromStream(stream);
+            }
+
+            return File(imageBytes, $"image/{output}");
+        }
+    }
+}
